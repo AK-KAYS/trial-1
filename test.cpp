e6777f7a2dd81f1aa240b2e4417d5ef5c18d88cd
AK-KAYS <mr.kanyanta@gmail.com>
2023-07-24 23:24:53 +0200

    changed

--- test.cpp
+++ test.cpp
@@ -2,15 +2,6 @@
 #include <fstream>
 using namespace std;
 int main (){
-int sales[] = {180,300,200,250,270};
-ofstream dataFile;
-dataFile.open("sales.txt");
-if (!dataFile)
-cout << "Error opening data file\n";
-else // output daily sales to a file
-{ for (int day = 0; day < 5; day++)
-dataFile << sales[day]<<endl;
-dataFile.close();
-}
+cout << "welcome fe............";
 return 0;
 }
